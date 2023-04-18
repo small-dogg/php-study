bfaedfff7e811fe09f86fdec72cb8a7b984aa9d5
small-dogg <sy920112@naver.com>
2023-04-18 22:30:25 +0900

    init: codeIgniter 초기화

---
+++ project-root/app/Controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController{
+    public function index(){
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+
+    }
+}
---
+++ project-root/app/Views/templates/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+<h1><?= esc($title) ?></h1>
+
+<em>&copy; 2021</em>
+</body>
+</html>
